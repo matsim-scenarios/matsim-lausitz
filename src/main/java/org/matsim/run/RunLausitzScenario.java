d257396211a163df8adebcf8ed6687f540692073
hzoerner <h.zoerner@campus.tu-berlin.de>
2023-02-15 09:43:36 +0100

    created class to filter community shape files

--- src/main/java/org/matsim/run/RunLausitzScenario.java
+++ src/main/java/org/matsim/run/RunLausitzScenario.java
@@ -24,6 +24,7 @@ import org.matsim.core.config.groups.PlansCalcRouteConfigGroup;
 import org.matsim.core.config.groups.VspExperimentalConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.run.analysis.CommunityFilter;
 import org.matsim.run.analysis.CommuterAnalysis;
 import picocli.CommandLine;
 
@@ -39,7 +40,7 @@ import java.util.Set;
 		CreateCountsFromBAStData.class
 })
 @MATSimApplication.Analysis({
-		TravelTimeAnalysis.class, LinkStats.class, CheckPopulation.class, CommuterAnalysis.class,
+		TravelTimeAnalysis.class, LinkStats.class, CheckPopulation.class, CommuterAnalysis.class, CommunityFilter.class
 })
 public class RunLausitzScenario extends MATSimApplication {
 
