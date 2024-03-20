25e4d2e55d8aaa271b074f461fd12ed2e3948d76
zlukich <shootnik@gmail.com>
2024-03-20 16:30:19 +0100

    create branch plus test

--- src/test/java/org/matsim/run/RunIntegrationTest.java
+++ src/test/java/org/matsim/run/RunIntegrationTest.java
@@ -3,7 +3,17 @@ package org.matsim.run;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.matsim.application.MATSimApplication;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.simwrapper.SimWrapperConfigGroup;
 import org.matsim.testcases.MatsimTestUtils;
+import org.matsim.utils.eventsfilecomparison.ComparisonResult;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RunIntegrationTest {
 
@@ -13,11 +23,28 @@ public class RunIntegrationTest {
 	@Test
 	public void runScenario() {
 
-		assert MATSimApplication.execute(LausitzScenario.class,
-			"--1pct",
-			"--iterations", "1",
-			"--output", utils.getOutputDirectory(),
-			"--config:controller.overwriteFiles=deleteDirectoryIfExists") == 0 : "Must return non error code";
+		String output = utils.getOutputDirectory();
+
+		Config config = ConfigUtils.loadConfig("input/v1.0/lausitz-v1.0-25pct.config.xml");
+
+		config.global().setNumberOfThreads(1);
+		config.qsim().setNumberOfThreads(1);
+		config.controller().setLastIteration(1);
+		config.controller().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+		config.controller().setOutputDirectory(output);
+
+		ConfigUtils.addOrGetModule(config, SimWrapperConfigGroup.class).defaultDashboards = SimWrapperConfigGroup.Mode.disabled;
+
+
+		MATSimApplication.execute(LausitzScenario.class, config, "run", "--1pct"
+		);
+
+
+		EventsUtils.assertEqualEventsFingerprint(
+				new File(utils.getOutputDirectory(), "lausitz-1pct.output_events.xml.gz"),
+				new File(utils.getInputDirectory(), "lausitz-1pct.output_events.fp.zst").toString()
+		);
+
 
 	}
 }
