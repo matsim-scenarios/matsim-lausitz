dd97bdd78ec8a6f66841d8e8c5e01505d3111805
simei94 <meinhardt@vsp.tu-berlin.de>
2024-05-27 16:54:38 +0200

    v1.x

--- src/main/java/org/matsim/run/LausitzScenario.java
+++ src/main/java/org/matsim/run/LausitzScenario.java
@@ -45,7 +45,7 @@ import java.util.Set;
 })
 public class LausitzScenario extends MATSimApplication {
 
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.x";
 
 	@CommandLine.Mixin
 	private final SampleOptions sample = new SampleOptions( 25, 10, 1);
