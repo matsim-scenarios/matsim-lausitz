e894ee81f94107e2d64edc584b40efabbdb355e9
sime94 <meinhardt@vsp.tu-berlin.de>
2024-09-03 16:01:36 +0200

    re-arrange drt configs

---
+++ src/main/java/org/matsim/run/DrtOptions.java
@@ -0,0 +1,140 @@
+package org.matsim.run;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.contrib.drt.optimizer.constraints.DefaultDrtOptimizationConstraintsSet;
+import org.matsim.contrib.drt.optimizer.constraints.DrtOptimizationConstraintsParams;
+import org.matsim.contrib.drt.optimizer.insertion.extensive.ExtensiveInsertionSearchParams;
+import org.matsim.contrib.drt.routing.DrtRoute;
+import org.matsim.contrib.drt.routing.DrtRouteFactory;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.drt.run.DrtConfigs;
+import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.config.groups.ScoringConfigGroup;
+import org.matsim.run.prepare.PrepareNetwork;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleCapacity;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.VehicleUtils;
+import picocli.CommandLine;
+
+import java.util.Set;
+
+/**
+ * This class bundles some run parameter options and functionalities connected to drt-scenarios.
+ */
+public class DrtOptions {
+	@CommandLine.Option(names = "--drt-shp", description = "Path to shp file for adding drt not network links as an allowed mode.", defaultValue = "./input/v1.1/drt-area/nord-bautzen-waiting-times_utm32N.shp")
+	private String drtAreaShp;
+
+	@CommandLine.Option(names = "--typ-wt", description = "typical waiting time", defaultValue = "300")
+	protected double typicalWaitTime;
+
+	@CommandLine.Option(names = "--wt-std", description = "waiting time standard deviation", defaultValue = "0.3")
+	protected double waitTimeStd;
+
+	@CommandLine.Option(names = "--ride-time-alpha", description = "ride time estimator alpha", defaultValue = "1.25")
+	protected double rideTimeAlpha;
+
+	@CommandLine.Option(names = "--ride-time-beta", description = "ride time estimator beta", defaultValue = "300")
+	protected double rideTimeBeta;
+
+	@CommandLine.Option(names = "--ride-time-std", description = "ride duration standard deviation", defaultValue = "0.3")
+	protected double rideTimeStd;
+
+	/**
+	 * a helper method, which makes all necessary config changes to simulate drt.
+	 */
+	void configureDrtConfig(Config config) {
+		DvrpConfigGroup dvrpConfigGroup = ConfigUtils.addOrGetModule(config, DvrpConfigGroup.class);
+		dvrpConfigGroup.networkModes = Set.of(TransportMode.drt);
+
+		MultiModeDrtConfigGroup multiModeDrtConfigGroup = ConfigUtils.addOrGetModule(config, MultiModeDrtConfigGroup.class);
+
+		if (multiModeDrtConfigGroup.getModalElements().isEmpty()) {
+			DrtConfigGroup drtConfigGroup = new DrtConfigGroup();
+			drtConfigGroup.operationalScheme = DrtConfigGroup.OperationalScheme.serviceAreaBased;
+			drtConfigGroup.stopDuration = 60.;
+			drtConfigGroup.drtServiceAreaShapeFile = getDrtAreaShp();
+
+//			optimization params now are in its own paramSet, hence the below lines
+			DrtOptimizationConstraintsParams optimizationConstraints = new DrtOptimizationConstraintsParams();
+			DefaultDrtOptimizationConstraintsSet optimizationConstraintsSet = new DefaultDrtOptimizationConstraintsSet();
+			optimizationConstraintsSet.maxWaitTime = 1200.;
+			optimizationConstraintsSet.maxTravelTimeBeta = 1200.;
+			optimizationConstraintsSet.maxTravelTimeAlpha = 1.5;
+			optimizationConstraints.addParameterSet(optimizationConstraintsSet);
+			drtConfigGroup.addParameterSet(optimizationConstraints);
+			drtConfigGroup.addParameterSet(new ExtensiveInsertionSearchParams());
+			multiModeDrtConfigGroup.addParameterSet(drtConfigGroup);
+		}
+
+		// set to drt estimate and teleport
+//		this enables the usage of the DrtEstimator by CL
+		for (DrtConfigGroup drtConfigGroup : multiModeDrtConfigGroup.getModalElements()) {
+			drtConfigGroup.simulationType = DrtConfigGroup.SimulationType.estimateAndTeleport;
+		}
+
+//		this is needed for DynAgents for DVRP
+		config.qsim().setSimStarttimeInterpretation(QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime);
+
+		ScoringConfigGroup scoringConfigGroup = ConfigUtils.addOrGetModule(config, ScoringConfigGroup.class);
+
+		if (!scoringConfigGroup.getModes().containsKey(TransportMode.drt)) {
+//			ASC drt = ASC pt as discussed in PHD seminar24
+//			add mode params for drt if missing and set ASC + marg utility of traveling = 0
+			scoringConfigGroup.addModeParams(new ScoringConfigGroup.ModeParams(TransportMode.drt)
+				.setConstant(scoringConfigGroup.getModes().get(TransportMode.pt).getConstant())
+				.setMarginalUtilityOfTraveling(-0.));
+		}
+
+//		creates a drt staging activity and adds it to the scoring params
+		DrtConfigs.adjustMultiModeDrtConfig(multiModeDrtConfigGroup, config.scoring(), config.routing());
+	}
+
+	/**
+	 * a helper method, which makes all necessary scenario changes to simulate drt.
+	 */
+	void configureDrtScenario(Scenario scenario) {
+
+		//		drt route factory has to be added as factory for drt routes, as there were no drt routes before.
+		scenario.getPopulation()
+			.getFactory()
+			.getRouteFactories()
+			.setRouteFactory(DrtRoute.class, new DrtRouteFactory());
+
+//		prepare network for drt
+		PrepareNetwork.prepareDrtNetwork(scenario.getNetwork(), getDrtAreaShp());
+		//		add drt veh type if not already existing
+		Id<VehicleType> drtTypeId = Id.create(TransportMode.drt, VehicleType.class);
+		if (!scenario.getVehicles().getVehicleTypes().containsKey(drtTypeId)) {
+//			drt veh type = car veh type, but capacity 1 passenger
+			VehicleType drtType = VehicleUtils.createVehicleType(drtTypeId);
+
+			VehicleUtils.copyFromTo(scenario.getVehicles().getVehicleTypes().get(Id.create(TransportMode.car, VehicleType.class)), drtType);
+			drtType.setDescription("drt vehicle copied from car vehicle type");
+			VehicleCapacity capacity = drtType.getCapacity();
+			capacity.setSeats(1);
+
+			scenario.getVehicles().addVehicleType(drtType);
+
+			Vehicle drtDummy = VehicleUtils.createVehicle(Id.createVehicleId("drtDummy"), drtType);
+			drtDummy.getAttributes().putAttribute("dvrpMode", TransportMode.drt);
+			drtDummy.getAttributes().putAttribute("startLink", "706048410#0");
+			drtDummy.getAttributes().putAttribute("serviceBeginTime", 0.);
+			drtDummy.getAttributes().putAttribute("serviceEndTime", 86400.);
+
+			scenario.getVehicles().addVehicle(drtDummy);
+		}
+	}
+
+	public String getDrtAreaShp() {
+		return drtAreaShp;
+	}
+
+}
--- src/main/java/org/matsim/run/RunLausitzDrtScenario.java
+++ src/main/java/org/matsim/run/RunLausitzDrtScenario.java
@@ -1,76 +1,44 @@
 package org.matsim.run;
 
-import org.locationtech.jts.geom.Geometry;
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.TransportMode;
-import org.matsim.api.core.v01.network.Link;
 import org.matsim.application.MATSimApplication;
-import org.matsim.application.options.ShpOptions;
 import org.matsim.contrib.drt.estimator.DrtEstimatorModule;
 import org.matsim.contrib.drt.estimator.impl.DirectTripBasedDrtEstimator;
 import org.matsim.contrib.drt.estimator.impl.distribution.NormalDistributionGenerator;
 import org.matsim.contrib.drt.estimator.impl.trip_estimation.ConstantRideDurationEstimator;
 import org.matsim.contrib.drt.estimator.impl.waiting_time_estimation.ConstantWaitingTimeEstimator;
-import org.matsim.contrib.drt.optimizer.constraints.DefaultDrtOptimizationConstraintsSet;
-import org.matsim.contrib.drt.optimizer.constraints.DrtOptimizationConstraintsParams;
-import org.matsim.contrib.drt.optimizer.insertion.extensive.ExtensiveInsertionSearchParams;
-import org.matsim.contrib.drt.routing.DrtRoute;
-import org.matsim.contrib.drt.routing.DrtRouteFactory;
 import org.matsim.contrib.drt.run.DrtConfigGroup;
-import org.matsim.contrib.drt.run.DrtConfigs;
 import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
 import org.matsim.contrib.drt.run.MultiModeDrtModule;
-import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.contrib.dvrp.run.DvrpModule;
 import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.QSimConfigGroup;
-import org.matsim.core.config.groups.ScoringConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.network.algorithms.MultimodalNetworkCleaner;
-import org.matsim.core.utils.geometry.geotools.MGC;
-import org.matsim.vehicles.Vehicle;
-import org.matsim.vehicles.VehicleCapacity;
-import org.matsim.vehicles.VehicleType;
-import org.matsim.vehicles.VehicleUtils;
 import picocli.CommandLine;
 
 import javax.annotation.Nullable;
-import java.util.*;
 
 /**
  * Run the Lausitz scenario including a regional DRT service.
  * All necessary configs will be made in this class.
  */
 public final class RunLausitzDrtScenario extends MATSimApplication {
-//	TODO: upload correct service area shp file to git
-	@CommandLine.Option(names = "--drt-shp", description = "Path to shp file for adding drt not network links as an allowed mode.", defaultValue = "./input/shp/lausitz.shp")
-	private String drtAreaShp;
 
-	@CommandLine.Option(names = "--typ-wt", description = "typical waiting time", defaultValue = "300")
-	private double typicalWaitTime;
-
-	@CommandLine.Option(names = "--wt-std", description = "waiting time standard deviation", defaultValue = "0.3")
-	private double waitTimeStd;
-
-	@CommandLine.Option(names = "--ride-time-alpha", description = "ride time estimator alpha", defaultValue = "1.25")
-	private double rideTimeAlpha;
-
-	@CommandLine.Option(names = "--ride-time-beta", description = "ride time estimator beta", defaultValue = "300")
-	private double rideTimeBeta;
-
-	@CommandLine.Option(names = "--ride-time-std", description = "ride duration standard deviation", defaultValue = "0.3")
-	private double rideTimeStd;
+//	run params re drt are contained in separate class DrtOptions
+	@CommandLine.ArgGroup(heading = "%nDrt options%n", exclusive = false, multiplicity = "0..1")
+	private final DrtOptions drtOpt = new DrtOptions();
 
 	private final LausitzScenario baseScenario = new LausitzScenario();
 
+//	this constructor is needed when this class is to be called from external classes with a given Config (e.g. for testing).
+	public RunLausitzDrtScenario(Config config) {
+		super(config);
+	}
+
 	public RunLausitzDrtScenario() {
-//		TODO: change this back to "automagic" version loading of config
-//		super(String.format("input/v%s/lausitz-v%s-10pct.config.xml", LausitzScenario.VERSION, LausitzScenario.VERSION));
-		super("input/v1.1/lausitz-v1.1-10pct.config.xml");
+		super(String.format("input/v%s/lausitz-v%s-10pct.config.xml", LausitzScenario.VERSION, LausitzScenario.VERSION));
 	}
 
 	public static void main(String[] args) {
@@ -83,49 +51,8 @@ public final class RunLausitzDrtScenario extends MATSimApplication {
 //		apply all config changes from base scenario class
 		baseScenario.prepareConfig(config);
 
-		DvrpConfigGroup dvrpConfigGroup = ConfigUtils.addOrGetModule(config, DvrpConfigGroup.class);
-		dvrpConfigGroup.networkModes = Set.of(TransportMode.drt);
-
-		MultiModeDrtConfigGroup multiModeDrtConfigGroup = ConfigUtils.addOrGetModule(config, MultiModeDrtConfigGroup.class);
-
-		if (multiModeDrtConfigGroup.getModalElements().isEmpty()) {
-			DrtConfigGroup drtConfigGroup = new DrtConfigGroup();
-			drtConfigGroup.operationalScheme = DrtConfigGroup.OperationalScheme.serviceAreaBased;
-			drtConfigGroup.stopDuration = 60.;
-			drtConfigGroup.drtServiceAreaShapeFile = drtAreaShp;
-
-//			optimization params now are in its own paramSet, hence the below lines
-			DrtOptimizationConstraintsParams optimizationConstraints = new DrtOptimizationConstraintsParams();
-			DefaultDrtOptimizationConstraintsSet optimizationConstraintsSet = new DefaultDrtOptimizationConstraintsSet();
-			optimizationConstraintsSet.maxWaitTime = 1200.;
-			optimizationConstraintsSet.maxTravelTimeBeta = 1200.;
-			optimizationConstraintsSet.maxTravelTimeAlpha = 1.5;
-			optimizationConstraints.addParameterSet(optimizationConstraintsSet);
-			drtConfigGroup.addParameterSet(optimizationConstraints);
-			drtConfigGroup.addParameterSet(new ExtensiveInsertionSearchParams());
-			multiModeDrtConfigGroup.addParameterSet(drtConfigGroup);
-		}
-
-		// set to drt estimate and teleport
-		for (DrtConfigGroup drtConfigGroup : multiModeDrtConfigGroup.getModalElements()) {
-			drtConfigGroup.simulationType = DrtConfigGroup.SimulationType.estimateAndTeleport;
-		}
-
-//		this is needed for DynAgents for DVRP
-		config.qsim().setSimStarttimeInterpretation(QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime);
-
-		ScoringConfigGroup scoringConfigGroup = ConfigUtils.addOrGetModule(config, ScoringConfigGroup.class);
-
-		if (!scoringConfigGroup.getModes().containsKey(TransportMode.drt)) {
-//			ASC drt = ASC pt as discussed in PHD seminar24
-//			add mode params for drt if missing and set ASC + marg utility of traveling = 0
-			scoringConfigGroup.addModeParams(new ScoringConfigGroup.ModeParams(TransportMode.drt)
-				.setConstant(scoringConfigGroup.getModes().get(TransportMode.pt).getConstant())
-				.setMarginalUtilityOfTraveling(-0.));
-		}
-
-//		creates a drt staging activity and adds it to the scoring params
-		DrtConfigs.adjustMultiModeDrtConfig(multiModeDrtConfigGroup, config.scoring(), config.routing());
+//		apply all necessary config changes for drt simulation
+		drtOpt.configureDrtConfig(config);
 
 		return config;
 	}
@@ -135,54 +62,8 @@ public final class RunLausitzDrtScenario extends MATSimApplication {
 //		apply all scenario changes from base scenario class
 		baseScenario.prepareScenario(scenario);
 
-//		drt route factory has to be added as factory for drt routes, as there were no drt routes before.
-		scenario.getPopulation()
-			.getFactory()
-			.getRouteFactories()
-			.setRouteFactory(DrtRoute.class, new DrtRouteFactory());
-
-//		add drt as allowed mode for whole Lausitz region
-		Geometry geometry = new ShpOptions(drtAreaShp, null, null).getGeometry();
-
-//		with the estimator, drt is teleported, but we may need drt as an allowed mode for
-//		separate drt post simulation
-		for (Link link : scenario.getNetwork().getLinks().values()) {
-			if (link.getAllowedModes().contains(TransportMode.car)) {
-				boolean isInside = MGC.coord2Point(link.getFromNode().getCoord()).within(geometry) ||
-					MGC.coord2Point(link.getToNode().getCoord()).within(geometry);
-
-				if (isInside) {
-					Set<String> modes = new HashSet<>();
-					modes.add(TransportMode.drt);
-					modes.addAll(link.getAllowedModes());
-					link.setAllowedModes(modes);
-				}
-			}
-		}
-		new MultimodalNetworkCleaner(scenario.getNetwork()).run(Set.of(TransportMode.drt));
-
-		Id<VehicleType> drtTypeId = Id.create(TransportMode.drt, VehicleType.class);
-
-//		add drt veh type if not already existing
-		if (!scenario.getVehicles().getVehicleTypes().containsKey(drtTypeId)) {
-//			drt veh type = car veh type, but capacity 1 passenger
-			VehicleType drtType = VehicleUtils.createVehicleType(drtTypeId);
-
-			VehicleUtils.copyFromTo(scenario.getVehicles().getVehicleTypes().get(Id.create(TransportMode.car, VehicleType.class)), drtType);
-			drtType.setDescription("drt vehicle copied from car vehicle type");
-			VehicleCapacity capacity = drtType.getCapacity();
-			capacity.setSeats(1);
-
-			scenario.getVehicles().addVehicleType(drtType);
-
-			Vehicle drtDummy = VehicleUtils.createVehicle(Id.createVehicleId("drtDummy"), drtType);
-			drtDummy.getAttributes().putAttribute("dvrpMode", TransportMode.drt);
-			drtDummy.getAttributes().putAttribute("startLink", "706048410#0");
-			drtDummy.getAttributes().putAttribute("serviceBeginTime", 0.);
-			drtDummy.getAttributes().putAttribute("serviceEndTime", 86400.);
-
-			scenario.getVehicles().addVehicle(drtDummy);
-		}
+//		apply all necessary scenario changes for drt simulation
+		drtOpt.configureDrtScenario(scenario);
 	}
 
 	@Override
@@ -207,10 +88,10 @@ public final class RunLausitzDrtScenario extends MATSimApplication {
 				public void install() {
 					DrtEstimatorModule.bindEstimator(binder(), drtConfigGroup.mode).toInstance(
 						new DirectTripBasedDrtEstimator.Builder()
-							.setWaitingTimeEstimator(new ConstantWaitingTimeEstimator(typicalWaitTime))
-							.setWaitingTimeDistributionGenerator(new NormalDistributionGenerator(1, waitTimeStd))
-							.setRideDurationEstimator(new ConstantRideDurationEstimator(rideTimeAlpha, rideTimeBeta))
-							.setRideDurationDistributionGenerator(new NormalDistributionGenerator(2, rideTimeStd))
+							.setWaitingTimeEstimator(new ConstantWaitingTimeEstimator(drtOpt.typicalWaitTime))
+							.setWaitingTimeDistributionGenerator(new NormalDistributionGenerator(1, drtOpt.waitTimeStd))
+							.setRideDurationEstimator(new ConstantRideDurationEstimator(drtOpt.rideTimeAlpha, drtOpt.rideTimeBeta))
+							.setRideDurationDistributionGenerator(new NormalDistributionGenerator(2, drtOpt.rideTimeStd))
 							.build()
 					);
 				}
