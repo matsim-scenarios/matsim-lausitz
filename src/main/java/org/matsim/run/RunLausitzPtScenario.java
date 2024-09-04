fe570f10e35b47afc2255b595e086396543e36d4
sime94 <meinhardt@vsp.tu-berlin.de>
2024-09-04 11:26:45 +0200

    checkstyle

--- src/main/java/org/matsim/run/RunLausitzPtScenario.java
+++ src/main/java/org/matsim/run/RunLausitzPtScenario.java
@@ -26,11 +26,11 @@ import java.util.*;
  */
 public class RunLausitzPtScenario extends MATSimApplication {
 
+	private final LausitzScenario baseScenario = new LausitzScenario();
+
 	public RunLausitzPtScenario(@Nullable Config config) {
 		super(config);
 	}
-
-	private final LausitzScenario baseScenario = new LausitzScenario();
 
 	public RunLausitzPtScenario() {
 		super(String.format("input/v%s/lausitz-v%s-10pct.config.xml", LausitzScenario.VERSION, LausitzScenario.VERSION));
