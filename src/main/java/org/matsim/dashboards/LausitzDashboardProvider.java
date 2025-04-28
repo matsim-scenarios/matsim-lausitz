03c5123cf12fae83274a6606c39c4e5ae29d683d
sime94 <meinhardt@vsp.tu-berlin.de>
2025-04-28 16:12:18 +0200

    correct svn paths

--- src/main/java/org/matsim/dashboards/LausitzDashboardProvider.java
+++ src/main/java/org/matsim/dashboards/LausitzDashboardProvider.java
@@ -26,8 +26,8 @@ public class LausitzDashboardProvider implements DashboardProvider {
 
 		return List.of(trips,
 			new EmissionsDashboard(config.global().getCoordinateSystem()),
-			new PtLineDashboard("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/lausitz/output/v2024.2/"),
-			new LausitzDrtDashboard("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/lausitz/output/v2024.2/",
+			new PtLineDashboard("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/lausitz/lausitz-v2024.2/output/10pct/"),
+			new LausitzDrtDashboard("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/lausitz/lausitz-v2024.2/output/10pct/",
 				config.global().getCoordinateSystem(), simWrapper.getConfigGroup().sampleSize)
 //			the NoiseAnalysis is not run here because it needs more RAM than the entire simulation,
 //			which leads to VM crashes and prevents other analysis to run. We have to run it separately (e.g. with LausitzSimWrapperRunner)
