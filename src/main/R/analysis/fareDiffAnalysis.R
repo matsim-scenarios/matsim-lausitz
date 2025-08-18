acf0f594fc4b4a290ab93074531b4d302017f5e1
sime94 <meinhardt@vsp.tu-berlin.de>
2025-08-18 13:10:24 +0200

    base case ctd full plans as reference

--- src/main/R/analysis/fareDiffAnalysis.R
+++ src/main/R/analysis/fareDiffAnalysis.R
@@ -21,7 +21,7 @@ run_dir_fixed <- gsub("////", "/", run_dir)
 
 # if you do not want to use opt_parse, comment out the above lines starting from option_list <- ...
 # you have to define run_dir_fixed yourself
-# run_dir_fixed <- "Y:/net/ils/matsim-lausitz/caseStudies/v2.0/drt-case-study/no-pooling-0-fare/output-4-ruhland-bhf-spremberg-bhf-schwarze-pumpe"
+# run_dir_fixed <- "Y:/net/ils/matsim-lausitz/v2.0-release/base-case-ctd-10-pct/output-lausitz-v2.0-10pct-base-case-ctd_full_plans"
 
 setwd(run_dir_fixed)
 print(paste("Running analysis on run dir", getwd()))
@@ -38,7 +38,7 @@ money_events_policy <- read_delim(file = money_events_path,delim = ";", col_type
     transactionPartner = col_character(),
     reference = col_character()))
 
-money_events_base <- read_delim(file = "Y:/net/ils/matsim-lausitz/v2.0-release/base-case-ctd-10-pct/output-lausitz-v2.0-10pct-base-case-ctd-commit-430893a/lausitz-v2.0-10pct-base-case-ctd.output_personMoneyEvents.tsv.gz",
+money_events_base <- read_delim(file = "Y:/net/ils/matsim-lausitz/v2.0-release/base-case-ctd-10-pct/output-lausitz-v2.0-10pct-base-case-ctd_full_plans/lausitz-v2.0-10pct-base-case-ctd_full_plans.output_personMoneyEvents.tsv.gz",
   delim = ";", col_types = cols(
     time = col_double(),
     person = col_character(),
