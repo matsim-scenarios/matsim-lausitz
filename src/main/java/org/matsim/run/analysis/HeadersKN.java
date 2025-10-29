43b96cfcf72a9186674fec534a891704350b0d9e
Kai Nagel <nagel@vsp.tu-berlin.de>
2025-10-29 09:05:59 +0100

    add AgentWiseComparisonKN

---
+++ src/main/java/org/matsim/run/analysis/HeadersKN.java
@@ -0,0 +1,9 @@
+package org.matsim.run.analysis;
+
+class HeadersKN{
+	public static String personId = "personId";
+	public static String income = "income";
+	public static String score = "score";
+	public static String utlOfMoney = "utlOfMoney";
+	public static String ttime = "ttime";
+}
